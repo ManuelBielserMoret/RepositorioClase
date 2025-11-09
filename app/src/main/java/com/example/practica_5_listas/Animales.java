81fac54b3f60a8a66758b79ad50a959b4a42d790
Manuel <mbm0035@alu.medac.es>
2025-11-09 13:33:33 +0100

    Paso 2.3
    Creamos la clase animal y le añadimos los atributos que queramos

---
+++ app/src/main/java/com/example/practica_5_listas/Animales.java
@@ -0,0 +1,14 @@
+package com.example.practica_5_listas;
+
+public class Animales {
+    private String nombre;
+    private String raza;
+    private int edad;
+
+    public Animales (String nombre, String raza, int edad) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.edad = edad;
+    }
+
+}
