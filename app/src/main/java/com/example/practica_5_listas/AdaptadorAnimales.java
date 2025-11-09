6b90a869363f8a41995a59d96bb27f67f32b8005
Manuel <mbm0035@alu.medac.es>
2025-11-09 13:54:15 +0100

    2.5 (error)
    Corregir un pequeño fallo

--- app/src/main/java/com/example/practica_5_listas/AdaptadorAnimales.java
+++ app/src/main/java/com/example/practica_5_listas/AdaptadorAnimales.java
@@ -3,8 +3,8 @@ package com.example.practica_5_listas;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 public class AdaptadorAnimales  extends RecyclerView.Adapter<AdaptadorAnimales.ViewHolder>{
-    private animales[] listaAnimales;
-    public AdaptadorAnimales (animales[] listaAnimales) {
+    private Animales[] listaAnimales;
+    public AdaptadorAnimales (Animales[] listaAnimales) {
         this.listaAnimales = listaAnimales;
 
     }
