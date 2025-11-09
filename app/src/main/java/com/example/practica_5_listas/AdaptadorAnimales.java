2b810f0be577b40e8fa33d4408965bef19d1871e
Manuel <mbm0035@alu.medac.es>
2025-11-09 14:06:08 +0100

    Paso 2.6

--- app/src/main/java/com/example/practica_5_listas/AdaptadorAnimales.java
+++ app/src/main/java/com/example/practica_5_listas/AdaptadorAnimales.java
@@ -1,11 +1,28 @@
 package com.example.practica_5_listas;
 
+import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
-import java.util.ArrayList;
-public class AdaptadorAnimales  extends RecyclerView.Adapter<AdaptadorAnimales.ViewHolder>{
+import android.view.View;
+public class AdaptadorAnimales extends RecyclerView.Adapter<AdaptadorAnimales.ViewHolder>{
     private Animales[] listaAnimales;
     public AdaptadorAnimales (Animales[] listaAnimales) {
         this.listaAnimales = listaAnimales;
 
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewNombre;
+        public TextView textViewRaza;
+        public TextView textViewEdad;
+        public ViewHolder(View view) {
+            super(view);
+            textViewNombre = view.findViewById(R.id.textViewNombre);
+            textViewRaza = view.findViewById(R.id.textViewRaza);
+            textViewEdad = view.findViewById(R.id.textViewEdad);
+        }
+        public void BindAnimal(Animales animal) {
+            textViewNombre.setText(animal.getNombre());
+            textViewRaza.setText(animal.getRaza());
+            textViewEdad.setText(String.valueOf(animal.getEdad()));
+        }
     }
 }
