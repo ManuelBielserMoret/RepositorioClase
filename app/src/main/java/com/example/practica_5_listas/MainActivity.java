082fae1579bafade0004c098ff3118587aa8ee36
Manuel <mbm0035@alu.medac.es>
2025-11-09 15:10:25 +0100

    Parte final de la practica + resultado

--- app/src/main/java/com/example/practica_5_listas/MainActivity.java
+++ app/src/main/java/com/example/practica_5_listas/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.practica_5_listas;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        recyclerView = findViewById(R.id.recView);
+        Animales[] misAnimales = new Animales[3];
+        misAnimales[0] = new Animales ("Rusty", "BordeCollie", 10);
+        misAnimales[1] = new Animales("Bruma", "Mastin blanco", 6);
+        misAnimales[2] = new Animales("Borreguito", "Terrier", 13);
+        AdaptadorAnimales adapter = new AdaptadorAnimales(misAnimales);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.recView), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
 }
